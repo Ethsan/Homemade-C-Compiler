f29a6fc91ac8ae12d7a81eb38f7c745f38e02dc0
Clément <Clement@Clement.local>
2024-01-06 14:46:41 +0100

    args

--- args.c
+++ args.c
@@ -11,7 +11,7 @@ void help(void)
 {
 	fprintf(stdout, "Usage: ./main [OPTION]... [FILE]...\n");
 	fprintf(stdout, "Options:\n");
-	/*liste des option
+	/*liste des options
 	-o : output
 	-h : help
 
@@ -23,13 +23,13 @@ void init_args(args_t *args)
 	args->input_file = NULL;
 	args->output_file = NULL;
 }
-int parse_args(int argc, char *argv[],args_t *args)
+int parse_args(int argc, char *argv[], args_t *args)
 {
 	int c;
 	int err = 0;
 	(void)err;
 
-	while ((c = getopt(argc, argv, "h:H:o:i:g:w:W:l:f:t:sda")) != -1) {
+	while ((c = getopt(argc, argv, "hi:o:")) != -1) {
 		switch (c) {
 		case 'i':
 			args->input_file = optarg;
@@ -37,24 +37,18 @@ int parse_args(int argc, char *argv[],args_t *args)
 		case 'o':
 			args->output_file = optarg;
 			break;
-                case 'h':
-                        help();
-                        exit(0);
+		case 'h':
+			help();
+			exit(0);
 		case '?':
 
 			if (optopt == 'o' || optopt == 'i') {
-				fprintf(stderr,
-					"Option -%c requires an argument.\n",
-					optopt);
+				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
 			} else {
 				if (isprint(optopt)) {
-					fprintf(stderr,
-						"Unknown option `-%c'.\n",
-						optopt);
+					fprintf(stderr, "Unknown option `-%c'.\n", optopt);
 				} else {
-					fprintf(stderr,
-						"Unknown option character `\\x%x'.\n",
-						optopt);
+					fprintf(stderr, "Unknown option character `\\x%x'.\n", optopt);
 				}
 			}
 			err = 1;
@@ -84,8 +78,12 @@ int check_args(args_t *args)
 		err = 1;
 	}
 	if (args->output_file == NULL) {
-		fprintf(stderr, "No output file\n");
-		err = 1;
+		args->output_file = malloc(sizeof(char) * 6);
+		strcpy(args->output_file, "out.s");
+		if (args->output_file == NULL) {
+			fprintf(stderr, "Error malloc\n");
+			exit(1);
+		}
 	}
 	if (err) {
 		help();
@@ -93,4 +91,3 @@ int check_args(args_t *args)
 	}
 	return 0;
 }
-
