6d66d4998a39d73a33428b32c2e48e5d637ba837
Clément <Clement@Clement.local>
2024-01-14 17:56:25 +0100

    nfuerzhnfi

---
+++ test/data/op_int.c
@@ -0,0 +1,16 @@
+int printint(int i);
+int printf(int *string);
+
+int main(){
+        int a, b,c;
+        a = 2;
+        b = 9;
+        printf("a : ");
+        printint(a);
+        printf("\n");
+        printf("b : ");
+        printint(b);
+        printf("\n");
+
+        
+}
